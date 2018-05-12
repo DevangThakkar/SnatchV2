c4d9199de5c9ca1a017ba8b23b2ca05b93a5d0c5
Devang <devang01121995@gmail.com>
2018-05-12 14:47:03 +0530

    possible removal of simultaneous typing

--- index.js
+++ index.js
@@ -1,52 +1,57 @@
 // Setup basic express server
-var express = require('express');
+var express = require("express");
 var app = express();
-var path = require('path');
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
+var path = require("path");
+var server = require("http").createServer(app);
+var io = require("socket.io")(server);
 var port = process.env.PORT || 3000;
-var PythonShell = require('python-shell');
-
-var centre = ' Centre: ';
-// Yes, I have removed the blanks because I was lazy; you may add them
-var bag = 'EEIBOOLEHALAPYTERWJRANEOSIDRRFLOESETAMQGIUVIUBPIKIFATRGNIADESTANHIDMGNEXTUOCRETLOISAZOYUODCNVNEAWE';
-var words = new Object();
-
-server.listen(port, () => {
-  console.log('Server listening at port %d', port);
-});
+var PythonShell = require("python-shell");
 
 // Routing
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Chatroom
-
 var numUsers = 0;
 
-io.on('connection', (socket) => {
+var centre = " Centre: ";
+// Yes, I have removed the blanks because I was lazy; you may add them
+var bag = "EEIBOOLEHALAPYTERWJRANEOSIDRRFLOESETAMQGIUVIUBPIKIFATRGNIADESTANHIDMGNEXTUOCRETLOISAZOYUODCNVNEAWE";
+var words = new Object();
+
+var active1 = True;
+var active2 = False;
+
+io.on("connection", (socket) => {
 	var addedUser = false;
 
-	// when the client emits 'new message', this listens and executes
-	socket.on('new message', (data) => {
-		// we tell the client to execute 'new message'
-		socket.broadcast.emit('new message', {
+	// when the client emits "new message", this listens and executes
+	socket.on("new message", (data) => {
+		// we tell the client to execute "new message"
+		socket.broadcast.emit("new message", {
 			username: socket.username,
 			message: data
 		});
+
+		while (active && active2){}
+
+		active1 = True;
+		active2 = True;
+
 		var options_c = {
-			mode: 'text',
+			mode: "text",
 			args: [centre.substring(8), data, socket.username, JSON.stringify(words)]
 		};
-		console.log("-"+JSON.stringify(words)+"-")
-		PythonShell.run('check.py', options_c, function (err, results) {
-			if (err) throw err;
+		PythonShell.run("check.py", options_c, function (err, results) {
+			if (err){
+				throw err;
+			}
 			// results is an array of messages collected during execution
-			console.log('results: %j', results);
-			
+			console.log("results: %j", results);
+
 			// if an invalid word is made
 			if (parseInt(results[0]) == -1){
 				centre = results[2];
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: results[1]+results[2]
 				});
@@ -54,10 +59,11 @@ io.on('connection', (socket) => {
 				for(var key in words){
 					str += key + ": " + words[key] + "; "
 				}
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: str
 				});
+				active1 = False;
 			}
 			// if a valid word is made
 			if (parseInt(results[0]) == 1){
@@ -66,7 +72,7 @@ io.on('connection', (socket) => {
 				word = word.replace(", ", "");
 				words[socket.username].push(word);
 				console.log(words);
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: results[1] + results[2]
 				});
@@ -74,24 +80,25 @@ io.on('connection', (socket) => {
 				for(var key in words){
 					str += key + ": " + words[key] + "; "
 				}
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: str
 				});
+				active1 = False;
 			}
 			// if valid tiles are drawn
 			if (parseInt(results[0]) == 2){
 				var options_d = {
-					mode: 'text',
+					mode: "text",
 					args: [centre.substring(8), bag, results[1], socket.username]
 				}
-				PythonShell.run('draw.py', options_d, function(err, results) {
+				PythonShell.run("draw.py", options_d, function(err, results) {
 					if (err) throw err;
 					// results is an array of messages collected during execution
-					console.log('results_d: %j', results);
+					console.log("results_d: %j", results);
 
 					if (parseInt(results[0]) == 0){
-						io.emit('new message', {
+						io.emit("new message", {
 							username: "snatch",
 							message: results[1] + results[2]
 						});
@@ -99,15 +106,16 @@ io.on('connection', (socket) => {
 						for(var key in words){
 							str += key + ": " + words[key] + "; "
 						}
-						io.emit('new message', {
+						io.emit("new message", {
 							username: "snatch",
 							message: str
 						});
+						active1 = False;
 					}
 
 					if (parseInt(results[0]) == 1){
 						centre = results[2]
-						io.emit('new message', {
+						io.emit("new message", {
 							username: "snatch",
 							message: results[1] + results[2]
 						});
@@ -115,18 +123,18 @@ io.on('connection', (socket) => {
 						for(var key in words){
 							str += key + ": " + words[key] + "; "
 						}
-						io.emit('new message', {
+						io.emit("new message", {
 							username: "snatch",
 							message: str
 						});
 						bag = results[3]
+						active1 = False;
 					}
-					
 				});
 			}
 
 			if(parseInt(results[0]) == -2){
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: results[1] + results[2]
 				});
@@ -134,17 +142,20 @@ io.on('connection', (socket) => {
 				for(var key in words){
 					str += key + ": " + words[key] + "; "
 				}
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: str
 				});
+				active1 = False;
 			}
 
-			if(parseInt(results[0]) == 0){}
+			if(parseInt(results[0]) == 0){
+				active1 = False;
+			}
 
 			if(parseInt(results[0]) == 3){
 				centre = results[2]
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: results[1] + results[2]
 				});
@@ -155,13 +166,15 @@ io.on('connection', (socket) => {
 				for(var key in words){
 					str += key + ": " + words[key] + "; "
 				}
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: str
 				});
+				active1 = False;
 			}
+
 			if(parseInt(results[0]) == -3){
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: results[1] + results[2]
 				});
@@ -169,15 +182,16 @@ io.on('connection', (socket) => {
 				for(var key in words){
 					str += key + ": " + words[key] + "; "
 				}
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: str
 				});
+				active1 = False;
 			}
 
 			if(parseInt(results[0]) == 4){
 				centre = results[2]
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: results[1] + results[2]
 				});
@@ -190,17 +204,18 @@ io.on('connection', (socket) => {
 				for(var key in words){
 					str += key + ": " + words[key] + "; "
 				}
-				io.emit('new message', {
+				io.emit("new message", {
 					username: "snatch",
 					message: str
 				});
+				active1 = False;
 			}
 
 		});
 	});
 
-	// when the client emits 'add user', this listens and executes
-	socket.on('add user', (username) => {
+	// when the client emits "add user", this listens and executes
+	socket.on("add user", (username) => {
 		if (addedUser)
 			return;
 
@@ -210,39 +225,39 @@ io.on('connection', (socket) => {
 		socket.username = username;
 		++numUsers;
 		addedUser = true;
-		socket.emit('login', {
+		socket.emit("login", {
 			numUsers: numUsers
 		});
 		// echo globally (all clients) that a person has connected
-		socket.broadcast.emit('user joined', {
+		socket.broadcast.emit("user joined", {
 			username: socket.username,
 			numUsers: numUsers
 		});
 	});
 
-	// when the client emits 'typing', we broadcast it to others
-	socket.on('typing', () => {
-		socket.broadcast.emit('typing', {
+	// when the client emits "typing", we broadcast it to others
+	socket.on("typing", () => {
+		socket.broadcast.emit("typing", {
 			username: socket.username
 		});
 	});
 
-	// when the client emits 'stop typing', we broadcast it to others
-	socket.on('stop typing', () => {
-		socket.broadcast.emit('stop typing', {
+	// when the client emits "stop typing", we broadcast it to others
+	socket.on("stop typing", () => {
+		socket.broadcast.emit("stop typing", {
 			username: socket.username
 		});
 	});
 
 	// when the user disconnects.. perform this
-	socket.on('disconnect', () => {
+	socket.on("disconnect", () => {
 		if (addedUser) {
 			--numUsers;
 
 			delete words[socket.username]
 
 			// echo globally that this client has left
-			socket.broadcast.emit('user left', {
+			socket.broadcast.emit("user left", {
 				username: socket.username,
 				numUsers: numUsers
 			});
