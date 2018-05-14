4d8d69dfb411733574bd66b44213fb23ac36cdc6
Devang <devang01121995@gmail.com>
2018-05-14 16:37:16 +0530

    faster dictionary support

--- check.py
+++ check.py
@@ -90,174 +90,119 @@ d2> /Ah. MB.
 d2> -TREATS
 >>> d2 withdrew TREATS; Centre: EYTREATS
 """
+# @profile
+def funct():
+	# first argument is the letters in the centre
+	centre = str(sys.argv[1]).upper().strip()
+	# second argument is the word attempted
+	attempted = str(sys.argv[2]).upper().strip()
+	# third argument is the user attempting the word
+	attempter = str(sys.argv[3]).strip()
+	#fourth argument is the words already made
+	words = str(sys.argv[4]).upper().strip()
 
-# first argument is the letters in the centre
-centre = str(sys.argv[1]).upper().strip()
-# second argument is the word attempted
-attempted = str(sys.argv[2]).upper().strip()
-# third argument is the user attempting the word
-attempter = str(sys.argv[3]).strip()
-#fourth argument is the words already made
-words = str(sys.argv[4]).strip()
+	words = words.replace("],","];")
+	for char in ["\"", "[", "]", "{", "}"]:
+		if char in words:
+			words = words.replace(char, "")
 
-words = words.replace("],","];")
-for char in ["\"", "[", "]", "{", "}"]:
-	if char in words:
-		words = words.replace(char, "")
-
-word_arr = words.split(";")
-word_dict = {}
-for user in word_arr:
-	username = user.strip().split(":")[0]
-	if username:
-		for word in ((user.strip().split(":"))[1]).strip().split(","):
-			if not username in word_dict:
-				word_dict[username] = [word]
-			else:
-				word_dict[username].append(word) 
+	word_arr = words.split(";")
+	word_dict = {}
+	for user in word_arr:
+		username = user.strip().split(":")[0]
+		if username:
+			for word in ((user.strip().split(":"))[1]).strip().split(","):
+				if not username in word_dict:
+					word_dict[username] = [word]
+				else:
+					word_dict[username].append(word) 
 
 
-check = True
-draw = False
-withdraw = False
-chat = False
-reset = False
+	check = True
+	draw = False
+	withdraw = False
+	chat = False
+	reset = False
 
-# DRAWING TILES
-if attempted[0] == '.':
-	check = False
-	draw = True
+	# DRAWING TILES
+	if attempted[0] == '.':
+		check = False
+		draw = True
 
-	for letter in attempted:
-		if letter != '.':
-			draw = False
+		for letter in attempted:
+			if letter != '.':
+				draw = False
 
-	if draw:
-		print("2")
-		print(len(attempted))
-	else:
-		print("-2")
-		print("Perhaps there's a typo; ")
-		print("Centre: " + centre)
-
-# CHATTING
-if attempted[0] == '/':
-	check = False
-	chat = True
-	print("0")
-	print(attempted[0][1:])
-
-# WITHDRAWING
-if attempted[0] == '-':
-	check = False
-	withdraw = True
-
-	flag_w = False
-	withdrawn = attempted[1:]
-	for user in word_dict:
-		for word in word_dict[user]:
-			if withdrawn == word:
-				print("3")
-				print(attempter+" has withdrawn "+user+"'s word "+word+"; ")
-				print("Centre: " + (centre+word))
-				print(user)
-				print(word)
-				flag_w = True
-			if flag_w:
-				break
-		if flag_w:
-			break
-
-	if not flag_w:
-		print("-3")
-		print("You are trying to withdraw a non existing word; ")
-		print("Centre: " + centre)
-
-#RESETTING
-if attempted[0] == '~':
-	check = False
-	reset = True
-	print("-9")
-	print(attempter + " has reset the game; ")
-	print("Centre: ")
-
-# CHECKING IF LEGIT WORD
-if check:
-	temp = centre
-	flag = True
-
-	for letter in attempted:
-		if letter not in centre:
-			flag = False
-			centre = temp
-			break
+		if draw:
+			print("2")
+			print(len(attempted))
 		else:
-			centre = centre.replace(letter, "", 1)
+			print("-2")
+			print("Perhaps there's a typo; ")
+			print("Centre: " + centre)
 
-	choice = attempted[0].upper()
-	fname = "updated_dict/CSW_" + choice + ".txt"
-	csw15 = set([])
-	with open(fname, "r") as f:
-		for line in f:
-			csw15.add(line.strip().rstrip())
+	# CHATTING
+	if attempted[0] == '/':
+		check = False
+		chat = True
+		print("0")
+		print(attempted[0][1:])
 
-	if ("\"" + attempted + "\"") not in csw15 and flag:
-		print("-1")
-		print(attempted + " is invalid; ")
-		centre = temp
-		print("Centre: " + centre)
-		flag = False
-		sys.exit()
+	# WITHDRAWING
+	if attempted[0] == '-':
+		check = False
+		withdraw = True
 
-	if flag:
-		print("1")
-		print(attempter + " made: " + attempted + ", ")
-		print("Centre: " + centre)
-		sys.exit()
-
-	if not flag:
-		flag1 = True
-		loser = ""
-		lost = ""
+		flag_w = False
+		withdrawn = attempted[1:]
 		for user in word_dict:
 			for word in word_dict[user]:
-				flag1 = True
-				temp_w = word
-				for letter in attempted:
-					if letter not in centre + word:
-						flag1 = False
-						centre = temp
-						break
-					else:
-						if letter in word:
-							word = word.replace(letter, "", 1)
-						else:
-							centre = centre.replace(letter, "", 1)
-				word = temp_w
-				temp_a = attempted
-				if flag1:
-					for letter in word:
-						if letter not in attempted:
-							flag1 = False
-							break
-						else:
-							attempted = attempted.replace(letter, "", 1)
-					attempted = temp_a
-
-				if flag1:
-					lost = word
+				if withdrawn == word:
+					print("3")
+					print(attempter+" has withdrawn "+user+"'s word "+word+"; ")
+					print("Centre: " + (centre+word))
+					print(user)
+					print(word)
+					flag_w = True
+				if flag_w:
 					break
-			if flag1:
-				loser = user
+			if flag_w:
 				break
-		if not flag1:
-			print("-1")
-			print(attempted + " is invalid; ")
-			centre = temp
+
+		if not flag_w:
+			print("-3")
+			print("You are trying to withdraw a non existing word; ")
 			print("Centre: " + centre)
-			sys.exit()
-		
-		if ("\"" + attempted + "\"") not in csw15 and flag1:
+
+	#RESETTING
+	if attempted[0] == '~':
+		check = False
+		reset = True
+		print("-9")
+		print(attempter + " has reset the game; ")
+		print("Centre: ")
+
+	# CHECKING IF LEGIT WORD
+	if check:
+		temp = centre
+		flag = True
+
+		for letter in attempted:
+			if letter not in centre:
+				flag = False
+				centre = temp
+				break
+			else:
+				centre = centre.replace(letter, "", 1)
+
+		choice = (attempted[0]+attempted[1]).upper()
+		csw15 = set([])
+		fname = "new_updated_dict/CSW_" + choice + ".txt"
+		with open(fname, "r") as f:
+			for line in f:
+				csw15.add(line.strip().rstrip())
+
+		if ("\"" + attempted + "\"") not in csw15 and flag:
 			print("-1")
 			print(attempted + " is invalid; ")
 			centre = temp
@@ -265,11 +210,69 @@ if check:
 			flag = False
 			sys.exit()
 
-		if flag1:
-			print("4")
-			print(attempter + " made: " + attempted + " from " + lost + "; ")
+		if flag:
+			print("1")
+			print(attempter + " made: " + attempted + ", ")
 			print("Centre: " + centre)
-			print(loser)
-			print(lost)
-			print(attempted)
 			sys.exit()
+
+		if not flag:
+			flag1 = True
+			loser = ""
+			lost = ""
+			for user in word_dict:
+				for word in word_dict[user]:
+					flag1 = True
+					temp_w = word
+					for letter in attempted:
+						if letter not in centre + word:
+							flag1 = False
+							centre = temp
+							break
+						else:
+							if letter in word:
+								word = word.replace(letter, "", 1)
+							else:
+								centre = centre.replace(letter, "", 1)
+					word = temp_w
+					temp_a = attempted
+					if flag1:
+						for letter in word:
+							if letter not in attempted:
+								flag1 = False
+								break
+							else:
+								attempted = attempted.replace(letter, "", 1)
+						attempted = temp_a
+
+					if flag1:
+						lost = word
+						break
+				if flag1:
+					loser = user
+					break
+			if not flag1:
+				print("-1")
+				print(attempted + " is invalid; ")
+				centre = temp
+				print("Centre: " + centre)
+				sys.exit()
+			
+			if ("\"" + attempted + "\"") not in csw15 and flag1:
+				print("-1")
+				print(attempted + " is invalid; ")
+				centre = temp
+				print("Centre: " + centre)
+				flag = False
+				sys.exit()
+
+			if flag1:
+				print("4")
+				print(attempter + " made: " + attempted + " from " + lost + "; ")
+				print("Centre: " + centre)
+				print(loser)
+				print(lost)
+				print(attempted)
+				sys.exit()
+
+funct()
