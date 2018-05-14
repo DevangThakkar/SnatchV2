f0be8b650624886ae090b6447cc08c299de64f3e
Devang <devang01121995@gmail.com>
2018-05-14 17:03:18 +0530

    snatching crashed, testing

--- index.js
+++ index.js
@@ -209,10 +209,8 @@ io.on("connection", (socket) => {
 				console.log(results[1])
 				words[socket.username].push(results[5]);
 				temp = words[results[3]]
-				io.emit("new message", {
-					username: "snatch",
-					message: results[3] + word[results[3]]
-				});
+				console.log(results[3])
+				console.log(word[results[3]])
 				temp.splice(temp.indexOf(results[4]), 1)
 				words[results[3]] = temp
 				var str = ""
