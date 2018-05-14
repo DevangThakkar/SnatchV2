fb7eed0ceaec47886d287d98a8e1bf4ac29b071f
Devang <devang01121995@gmail.com>
2018-05-14 17:05:47 +0530

    snatching crashed, testing

--- check.py
+++ check.py
@@ -278,7 +278,7 @@ def funct():
 				print("4")
 				print(attempter + " made: " + attempted + " from " + lost + "; ")
 				print("Centre: " + centre)
-				print(word_dict[loser])
+				print(loser)
 				print(lost)
 				print(attempted)
 				sys.exit()
